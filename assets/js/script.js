3de91d590078d1692178e506f486410efb293c89
idjjurado <ismael@eduphoria.net>
2023-04-25 22:08:13 -0600

    Adding in apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,31 @@
-// Tasty API URL
-const url = 'https://tasty.p.rapidapi.com/recipes/list';
-const options = {
+const settingsTasty = {
+	async: true,
+	crossDomain: true,
+	url: 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes',
 	method: 'GET',
 	headers: {
-		'content-type': 'application/octet-stream',
-		'X-RapidAPI-Key': 'fb86f9c2b8mshf93f18832749770p146adajsn70f18f6fb0ba',
+		'content-type': 'application/json',
+		'X-RapidAPI-Key': '8fba490658msh30c9e8c9a48dc6bp15f465jsnfa24de2a900b',
 		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
 	}
 };
 
-const response = fetch(url, options);
-const result = JSON.parse(response);
-console.log(result);
-// const analyzeComment = (comment, callback) => {
-//     // Creating an object to send to the server
-//     const data = {
-//             text: comment
-//             , language: 'english'
-//     };
-//     // Encoding data for application/x-www-form-urlencoded content type
-//     const formattedData = Qs.stringify(data);
-//     // POST request to server
-//     axios.post(API_URL, formattedData, { headers: REQUEST_HEADERS })
-//             .then(response => {
-//                const data = response.data;
-//               // Calling a callback function with data from the server
-//               callback(data)
-//             })
-//             .catch(error => console.error(error))
-//   };
+$.ajax(settingsTasty).done(function (response) {
+	console.log(response);
+});
 
-//   const displayResult = result => {
-//     // Remove invisible class for main-result-block
-//     const resultBlockElement = document.getElementById('main-result-block');
-//     resultBlockElement.classList.remove('invisible');
-  
-//     // Setting the color of the result text depending on the response label
-//     const label = result.label;
-//     const resultElement = document.getElementById('result');
-//     resultElement.setAttribute('class', label);
-//     let resultText = '';
-  
-//     // Choosing the result text depending on response label
-//     switch (label) {
-//         case 'pos':
-//             resultText = 'Wow! Your comment is very positive!';
-//             break;
-//         case 'neg':
-//             resultText = 'Negative comment =(';
-//             break;
-//         case 'neutral':
-//             resultText = 'Simple comment =)';
-//             break;
-//         default:
-//             resultText = "Hmmm, can't understand your comment";
-//     }
-  
-//     // Setting the result text
-//     resultElement.textContent = resultText;
-//   };  
+const settingsYuumly = {
+	async: true,
+	crossDomain: true,
+	url: 'https://yummly2.p.rapidapi.com/feeds/list?limit=24&start=0',
+	method: 'GET',
+	headers: {
+		'content-type': 'application/json',
+		'X-RapidAPI-Key': '8fba490658msh30c9e8c9a48dc6bp15f465jsnfa24de2a900b',
+		'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
+	}
+};
 
-//   const handleEmptyComment = () => {
-//     const resultBlockElement = document.getElementById('main-result-block');
-//     resultBlockElement.classList.add('invisible');
-//     return alert('Your comment is empty =(');
-//   };
-
-//   // Button click handler
-// const onAnalyzeButtonClick = () => {
-//     // Getting a textarea element with a comment
-//     const commentElement = document.getElementById('comment');
-//     // Getting comment text
-//     const commentText = commentElement.value.trim();
-  
-//     // Handle empty comment
-//     if (!commentText) {
-//             return handleEmptyComment();
-//     }
-//     // Calling the API and passing the result with the displayResult as a callback function
-//     return analyzeComment(commentText, displayResult);
-//   };
+$.ajax(settingsYuumly).done(function (response) {
+	console.log(response);
+});
