2afe24c9b5c7e9412277089a346f093b3063e8cf
Trenton_Sallee <tretnonsmiles@gmail.com>
2023-05-01 21:42:35 -0700

    displaying saved foods on front list

--- assets/js/umami.js
+++ assets/js/umami.js
@@ -101,6 +101,8 @@ const animeIds = [
 	"11741"
 ]
 
+let recipeSave = {}
+let recipeSaveButtonText = $("#saveRecipe").text()
 
 const settingsRandommeal = {
 	async: true,
@@ -125,6 +127,8 @@ const settingsRandommeal = {
 		$("#instruction-block").text(recipeInfo.strInstructions);
 		$("#recipe-video").attr(recipeInfo.strYoutube);
 
+		recipeSave ["recipeName"] = recipeInfo.strMeal
+		recipeSave ["ingredients"] = []
 
 		for (var i = 1; i < 20; i++) {
 			if (recipeInfo["strIngredient" + i].length > 0) {
@@ -135,11 +139,30 @@ const settingsRandommeal = {
 				ingredList.textContent = cookingMeasure + " " + ingredName;
 				addList.appendChild(ingredList);
 
+				recipeSave.ingredients.push({"cookingMeasure":cookingMeasure, "ingredName":ingredName})
+
 				console.log(recipeInfo["strIngredient" + i].length > 0);
 			}
-		}
+		} console.log(recipeSave)
 
 	});
+
+
+	$("#saveRecipe").click(function(){
+		if (recipeSaveButtonText === "recipe Saved!"){
+			return
+		}
+		let recipeSaveList = JSON.parse(localStorage.getItem("savedRecipes")) 
+		if (!recipeSaveList){
+			recipeSaveList = []
+		}
+		recipeSaveList.push(recipeSave)
+
+		localStorage.setItem("savedRecipes",JSON.stringify(recipeSaveList))
+		recipeSave = {};
+		$("#saveRecipe").text("recipe Saved!")
+	});
+
 
 	function sample(arr) {
 		const index = Math.floor(Math.random()*arr.length)
